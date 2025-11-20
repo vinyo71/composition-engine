7984bae12772c825a2d15c498e60f0ccd6e7480b
vinyo71 <eaviktor17@gmail.com>
2025-11-20 10:09:47 +0100

    datasheet generation with a new template

---
+++ debug_datasheet.ts
@@ -0,0 +1,11 @@
+
+import { parseXml, findRecords } from "./src/xml.ts";
+
+const xmlText = await Deno.readTextFile("inp/datasheet.xml");
+const root = parseXml(xmlText);
+console.log("Root keys:", Object.keys(root));
+console.log("Root structure:", JSON.stringify(root, null, 2));
+
+const records = findRecords(root, ".");
+console.log("Record 0 keys:", Object.keys(records[0]));
+console.log("Record 0 structure:", JSON.stringify(records[0], null, 2));
